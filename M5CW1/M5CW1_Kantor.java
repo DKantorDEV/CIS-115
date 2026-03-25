9256cf70e8f986c56bab16ffa1da17c8ed632654
DKantor2002 <d.kantor.dev@gmail.com>
2026-03-25 10:20:33 -0400

    Completed M5CW1

---
+++ M5CW1/M5CW1_Kantor.java
@@ -0,0 +1,30 @@
+// CIS-115
+// Daniel Kantor
+// M5CW1
+// This program uses a for loop to add numbers together
+
+import java.util.Scanner;
+
+public class M5CW1_Kantor {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Total tracker outside of for loop
+        int total = 0;
+
+        // For loop logic
+        for (int i=1; i < 6; i++) {
+            total += i;
+        }
+
+        String statement = "The sum of the numbers is: " + total;
+        
+        System.out.println(statement);
+
+        scanner.close();
+
+    }
+
+}
