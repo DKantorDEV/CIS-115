bb53e67795aca54be1a92920a0f0e9679220c8b0
DKantorDEV <d.kantor.dev@gmail.com>
2026-04-03 09:23:30 -0400

    Added comments

--- M5HW1/M5HW1_Kantor.java
+++ M5HW1/M5HW1_Kantor.java
@@ -6,20 +6,27 @@
 import java.util.Scanner;
 
 public class M5HW1_Kantor {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
 
+        // Variables declared here + scanner implemented
         char grade;
         Scanner k  = new Scanner(System.in);
         String runProgram = "y";
 
+        // While loop to run the program again or not (yes for first run)
         while (runProgram.equalsIgnoreCase("y")) {
 
             System.out.print("How many grades to calculate? ");
             int totalGrades = k.nextInt();
 
+            // Variables to keep track of grades to input and total for average
             double total = 0;
             int count = 0;
 
+            // Do while loop for the logic of the grade input
             do {
                 System.out.print("Enter grade " + (count + 1) + ": ");
                 double gradeInput = k.nextDouble();
@@ -33,11 +40,14 @@ public class M5HW1_Kantor {
                 total += gradeInput;
                 count ++;
 
+            // While loop continues so long as count is less than totalGrades (to ensure all grades get inputted)
             } while (count < totalGrades);
 
+            // Calculates the average
             double average = total / totalGrades;
             System.out.printf("Average: %.1f%n", average);
 
+            // If statements for what the grade will be
             if (average >= 90) {
                 grade = 'A';
             } else if (average >= 80) {
